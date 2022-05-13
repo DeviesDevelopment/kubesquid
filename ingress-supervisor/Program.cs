35eac8d4165dbc99afbbcb88a8aa6520067f8c4d
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-13 11:09:23 +0200

    use correct list pod method

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -18,5 +18,5 @@ Console.WriteLine($"Namespaces: {ns}");
 
 var client = new Kubernetes(config);
 
-var pods = client.CoreV1.ListPods();
+var pods = client.CoreV1.ListPodForAllNamespaces();
 Console.WriteLine(pods);
