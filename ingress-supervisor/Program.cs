2d4ba76cb46a9b079c2b881ecc683520f1d2ee6e
Rickard Andersson <rickard.andersson@devies.se>
2022-05-13 10:45:12 +0200

    list files in credentials mount

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -5,6 +5,9 @@ Console.WriteLine("Hello, World!");
 var config = KubernetesClientConfiguration.InClusterConfig();
 Console.WriteLine(config.Username);
 Console.WriteLine(config.Namespace);
+
+Console.WriteLine(Directory.GetFiles("/var/run/secrets/kubernetes.io/serviceaccount"));
+
 var client = new Kubernetes(config);
 
 var namespaces = client.CoreV1.ListNamespace();
