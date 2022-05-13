18a9ba760cc1fc74a259d0ceb34307eea48d4a53
Dunklas <dunklas@gmail.com>
2022-05-13 14:01:10 +0200

    Watch for kubernetes changes (#6)

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using k8s;
+using k8s.Models;
+
+Console.WriteLine("Hello, World!");
+
+var config = KubernetesClientConfiguration.InClusterConfig();
+var client = new Kubernetes(config);
+
+var servicesListResp = client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync("default", watch: true);
+await foreach (var (type, item) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
+{
+    Console.WriteLine("==on watch event==");
+    Console.WriteLine(type);
+    Console.WriteLine(item.Metadata.Name);
+    Console.WriteLine("==on watch event==");
+}   
+
