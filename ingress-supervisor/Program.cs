a49a6d7239360861e6e961a666adfbb35fdc0ea4
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-13 13:36:47 +0200

    start watching services

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -1,10 +1,17 @@
 ﻿using k8s;
+using k8s.Models;
 
 Console.WriteLine("Hello, World!");
 
 var config = KubernetesClientConfiguration.InClusterConfig();
 var client = new Kubernetes(config);
 
-var pods = client.CoreV1.ListNamespacedPod("default");
-Console.WriteLine(string.Join(",", pods));
+var servicesListResp = client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync("default", watch: true);
+await foreach (var (type, item) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
+{
+    Console.WriteLine("==on watch event==");
+    Console.WriteLine(type);
+    Console.WriteLine(item.Metadata.Name);
+    Console.WriteLine("==on watch event==");
+}   
 
