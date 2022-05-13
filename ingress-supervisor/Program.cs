6f7973223f77dfd87955fddf364fc81bb9092bfd
Rickard Andersson <rickard.andersson@devies.se>
2022-05-13 09:51:46 +0200

    List namespaces

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using k8s;
+
+Console.WriteLine("Hello, World!");
+
+var config = KubernetesClientConfiguration.InClusterConfig();
+var client = new Kubernetes(config);
+
+var namespaces = client.CoreV1.ListNamespace();
+Console.WriteLine(namespaces);
