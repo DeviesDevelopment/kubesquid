6fc1155cc64ca95a4f933c2a3d7f9e9bd4aa6af1
Rickard Andersson <rickard.andersson@devies.se>
2022-05-13 10:31:31 +0200

    Print username and namespace

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -3,6 +3,8 @@
 Console.WriteLine("Hello, World!");
 
 var config = KubernetesClientConfiguration.InClusterConfig();
+Console.WriteLine(config.Username);
+Console.WriteLine(config.Namespace);
 var client = new Kubernetes(config);
 
 var namespaces = client.CoreV1.ListNamespace();
