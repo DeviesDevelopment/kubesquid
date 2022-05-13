92b414383aa0de06b60e70a7330e09754931f6c9
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-13 13:22:25 +0200

    clean up program.cs

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -3,20 +3,8 @@
 Console.WriteLine("Hello, World!");
 
 var config = KubernetesClientConfiguration.InClusterConfig();
-Console.WriteLine(config.CurrentContext);
-Console.WriteLine(config.Host);
-Console.WriteLine(config.AccessToken);
-Console.WriteLine(config.Username);
-Console.WriteLine(config.Namespace);
-
-var files = Directory.GetFiles("/var/run/secrets/kubernetes.io/serviceaccount");
-Console.WriteLine(string.Join(",", files));
-string token = System.IO.File.ReadAllText(@"/var/run/secrets/kubernetes.io/serviceaccount/token");
-string ns = System.IO.File.ReadAllText(@"/var/run/secrets/kubernetes.io/serviceaccount/namespace");
-Console.WriteLine($"Token: {token}");
-Console.WriteLine($"Namespaces: {ns}");
-
 var client = new Kubernetes(config);
 
 var pods = client.CoreV1.ListNamespacedPod("default");
-Console.WriteLine(pods);
+Console.WriteLine(string.Join(",", pods));
+
