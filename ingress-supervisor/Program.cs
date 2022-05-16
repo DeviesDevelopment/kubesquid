749bce06368abf0b0f0c403bde39a1760021a436
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-16 15:41:19 +0200

    Start watching ConfigMap (#8)

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -2,15 +2,43 @@
 using k8s.Models;
 
 Console.WriteLine("Hello, World!");
-
 var config = KubernetesClientConfiguration.InClusterConfig();
 var client = new Kubernetes(config);
-var servicesListResp = client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync("default", watch: true);
-await foreach (var (type, item) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
-{
-    Console.WriteLine("==on watch event==");
-    Console.WriteLine(type);
-    Console.WriteLine(item.Metadata.Name);
-    Console.WriteLine("==on watch event==");
+var servicesListResp = client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync(config.Namespace, watch: true);
+var configMapResp = client.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync(config.Namespace, watch: true);
+
+async Task WatchServices() 
+{ 
+    Console.WriteLine("Staring to watch services");
+    await foreach (var (type, service) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
+    {
+        Console.WriteLine("==Watching Service Events==");
+        Console.WriteLine(type);
+        Console.WriteLine(service.Metadata.Name);
+        Console.WriteLine("==Watching Service Events==");
+    }
 }
 
+async Task WatchConfigMap()
+{
+    Console.WriteLine("Starting to watch the config map");
+    await foreach (var (type, configMap) in configMapResp.WatchAsync<V1ConfigMap, V1ConfigMapList>())
+    {
+        Console.WriteLine("==Watching ConfigMap Events==");
+        Console.WriteLine(type);
+        Console.WriteLine(configMap.Metadata.Name);
+        Console.WriteLine("==Watching ConfigMap Events==");
+    }
+}
+try
+{
+    Task.WaitAll(new[] { WatchServices(), WatchConfigMap() });
+}
+catch (AggregateException e)
+{ 
+    Console.WriteLine("An exception occured in one of our background tasks");
+    Console.WriteLine(e.Message); 
+}
+
+
+Console.WriteLine("Bye, World!");
