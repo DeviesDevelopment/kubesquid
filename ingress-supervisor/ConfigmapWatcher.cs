e212ce229039b681e23a9bf9b71d1f610d0df779
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-17 10:46:35 +0200

    Add ConfigmapWatcher

---
+++ ingress-supervisor/ConfigmapWatcher.cs
@@ -0,0 +1,44 @@
+using k8s;
+using k8s.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace DefaultNamespace;
+
+public class ConfigmapWatcher : BackgroundService
+{
+    private readonly Kubernetes _client;
+    private readonly string _targetNamespace;
+
+    public ConfigmapWatcher(Kubernetes client, string targetNamespace)
+    {
+        _client = client;
+        _targetNamespace = targetNamespace;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await WatchConfigmaps();
+        }
+        // TODO: Clean up
+    } 
+    private async Task WatchConfigmaps() 
+    {
+        var configMapResp = _client.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync(_targetNamespace, watch: true);
+        Console.WriteLine("Starting to watch the config map");
+        await foreach (var (type, configMap) in configMapResp.WatchAsync<V1ConfigMap, V1ConfigMapList>())
+        {
+            Console.WriteLine("==Watching ConfigMap Events==");
+            Console.WriteLine(type);
+            Console.WriteLine(configMap.Metadata.Name);
+            Console.WriteLine("==Watching ConfigMap Events==");
+
+            var configMapContent = await _client.CoreV1.ReadNamespacedConfigMapWithHttpMessagesAsync(configMap.Metadata.Name, _targetNamespace);
+            foreach (var key in configMapContent.Body.Data.Keys)
+            {
+                Console.WriteLine($"{key}: {configMapContent.Body.Data[key]}");
+            }
+        }
+    }
+}
--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -11,6 +11,7 @@ var client = new Kubernetes(config);
 var builder = new HostBuilder().ConfigureServices((hostContext, services) =>
 {
     services.AddSingleton<IHostedService>(x => new ServiceWatcher(client, config.Namespace));
+    services.AddSingleton<IHostedService>(x => new ConfigmapWatcher(client, config.Namespace));
 });
 
 await builder.RunConsoleAsync();
