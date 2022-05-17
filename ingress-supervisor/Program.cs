5bd2969cf1bd472d22f2eff7a09ab6187bc9dae9
Dunklas <dunklas@gmail.com>
2022-05-17 10:10:56 +0200

    Read and print content of configmaps (#9)

--- ingress-supervisor/Program.cs
+++ ingress-supervisor/Program.cs
@@ -1,4 +1,5 @@
-﻿using k8s;
+﻿using ingress_supervisor;
+using k8s;
 using k8s.Models;
 
 Console.WriteLine("Hello, World!");
@@ -6,12 +7,21 @@ var config = KubernetesClientConfiguration.InClusterConfig();
 var client = new Kubernetes(config);
 var servicesListResp = client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync(config.Namespace, watch: true);
 var configMapResp = client.CoreV1.ListNamespacedConfigMapWithHttpMessagesAsync(config.Namespace, watch: true);
+var kubernetesClient = new KubernetesWrapper();
 
 async Task WatchServices() 
 { 
     Console.WriteLine("Staring to watch services");
     await foreach (var (type, service) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
     {
+        switch (type)
+        {
+            case WatchEventType.Added:
+                
+                break;
+            default:
+                break;
+        }
         Console.WriteLine("==Watching Service Events==");
         Console.WriteLine(type);
         Console.WriteLine(service.Metadata.Name);
@@ -28,6 +38,12 @@ async Task WatchConfigMap()
         Console.WriteLine(type);
         Console.WriteLine(configMap.Metadata.Name);
         Console.WriteLine("==Watching ConfigMap Events==");
+
+        var configMapContent = await client.CoreV1.ReadNamespacedConfigMapWithHttpMessagesAsync(configMap.Metadata.Name, config.Namespace);
+        foreach (var key in configMapContent.Body.Data.Keys)
+        {
+            Console.WriteLine($"{key}: {configMapContent.Body.Data[key]}");
+        }
     }
 }
 try
