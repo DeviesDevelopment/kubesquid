3c9656dc4aad0074ab3ee6c4872d70f99d59c72f
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-18 10:14:28 +0200

    add start of e2e test script

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 
 Run the same application API for multiple tenants smoothly in kubernetes
 
+## Running e2e tests
+
+From `/tests` run
+
+```
+./e2e-test.sh
+```
+
+### Requirements
+
+- kind TODO: Version
+- kubectl TODO: Version
+- helm: TODO: Version
---
+++ tests/e2e-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 1. Spin up minikube / kind
+# 2. Validate that ingress controller exists
+# 3. Package helm
+# 4. Install helm package
+# 5. Add configmap
+# 6. Deploy service (whoami)
+# 7. Verify ingress created with correct parameters.
+
+# Terminate if any command fails
+set -e
+
+kind delete cluster
+# Create cluster with configuration allowing an Ingress Controller
+kind create cluster --config=./cluster-config.yml
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=90s
+
+kubectl version
+docker build --no-cache -t miledevies/kubesquid-ingress-supervisor:e2e-test ../ingress-supervisor
+kind load docker-image miledevies/kubesquid-ingress-supervisor:e2e-test --name kind
+helm package ../ingress-supervisor/kubesquid-ingress-supervisor
+helm install kubesquid-ingress-supervisor kubesquid-ingress-supervisor-0.1.0.tgz
