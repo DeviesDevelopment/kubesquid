e16632008e03e26ab2dc64b3bd2ee8db8e9e2893
Fredrik Mile <fredrik.mile@gmail.com>
2022-05-19 15:58:47 +0200

    Use logger instead of System.WriteLine

--- ingress-supervisor/ServiceWatcher.cs
+++ ingress-supervisor/ServiceWatcher.cs
@@ -2,6 +2,7 @@ using ingress_supervisor;
 using k8s;
 using k8s.Models;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace DefaultNamespace;
 
@@ -11,13 +12,15 @@ public class ServiceWatcher : BackgroundService
     private readonly string _targetNamespace;
     private readonly KubernetesWrapper _kubernetesWrapper;
     private readonly Logic _logic;
+    private readonly ILogger<ServiceWatcher> _logger;
 
-    public ServiceWatcher(Kubernetes client, KubernetesClientConfiguration config, KubernetesWrapper kubernetesWrapper, Logic logic)
+    public ServiceWatcher(Kubernetes client, KubernetesClientConfiguration config, KubernetesWrapper kubernetesWrapper, Logic logic, ILogger<ServiceWatcher> logger)
     {
         _client = client;
         _targetNamespace = config.Namespace;
         _kubernetesWrapper = kubernetesWrapper;
         _logic = logic;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -38,13 +41,13 @@ public class ServiceWatcher : BackgroundService
     private async Task WatchServices()
     {
         var servicesListResp = _client.CoreV1.ListNamespacedServiceWithHttpMessagesAsync(_targetNamespace, watch: true);
-        Console.WriteLine("Staring to watch services");
+        _logger.LogInformation("Starting to watch services");
         await foreach (var (type, service) in servicesListResp.WatchAsync<V1Service, V1ServiceList>())
         {
-            Console.WriteLine($"Got Service Event of Type: {type} for Service: {service.Metadata.Name}");
+            _logger.LogInformation("Got Service Event of Type: {} for Service: {}", type, service.Metadata.Name);
             if (service.Metadata?.Annotations?.ContainsKey("squid") != true)
             {
-                Console.WriteLine($"Ignoring service {service.Metadata.Name} due to missing squid annotation");
+                _logger.LogInformation("Ignoring service {} due to missing squid annotation", service.Metadata.Name);
                 continue;
             }
 
@@ -62,7 +65,7 @@ public class ServiceWatcher : BackgroundService
                         if (!_logic.ServiceHasIngress(allIngresses, serviceConfig))
                         {
                             await _kubernetesWrapper.CreateIngress(serviceConfig);
-                            Console.WriteLine($"Created ingress for service: {serviceConfig.ServiceName}");
+                            _logger.LogInformation("Created ingress for service: {}", service.Metadata.Name);
                         }
                     }
                     break;
@@ -72,7 +75,7 @@ public class ServiceWatcher : BackgroundService
                         if (_logic.ServiceHasIngress(allIngresses, serviceConfig))
                         {
                             await _kubernetesWrapper.DeleteIngress(serviceConfig);
-                            Console.WriteLine($"Deleted ingress for service: {serviceConfig.ServiceName}");
+                            _logger.LogInformation("Deleted ingress for service: {}", serviceConfig.ServiceName);
                         }
                     }
                     break;
