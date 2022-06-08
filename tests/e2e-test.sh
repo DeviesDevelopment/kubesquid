65ffc245aa05af79ccd811dc4afd600586b548d9
Fredrik Mile <fredrik.mile@gmail.com>
2022-06-08 15:48:33 +0200

    fix path in e2e tests

--- tests/e2e-test.sh
+++ tests/e2e-test.sh
@@ -17,7 +17,7 @@ done
 # Verify that cluster is running
 kubectl version
 kind load docker-image deviesdevelopment/kubesquid-ingress-supervisor:"$IMAGE_TAG" --name kind
-helm package ../ingress-supervisor/kubesquid-ingress-supervisor
+helm package ../charts/kubesquid-ingress-supervisor
 helm install --set image.tag="$IMAGE_TAG" kubesquid-ingress-supervisor kubesquid-ingress-supervisor-0.1.0.tgz
 kubectl apply -f test-configmap.yml
 # Install whoami service and annotate it with "squid"
